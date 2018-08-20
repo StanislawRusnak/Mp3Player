1300d12b2efd2ed73c174d3b824b02e4c0adb52e
Stanislaw Rusnak <stanislaw.rusnak1@gmail.com>
2018-08-20 22:14:29 +0200

    Mp3Player

---
+++ build/build/src/pl/javastart/mp3Player/controller/MenuPaneController.java
@@ -0,0 +1,81 @@
+package pl.javastart.mp3Player.controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
+public class MenuPaneController implements Initializable {
+
+    @FXML
+    private Menu fileMenu;
+
+    @FXML
+    private MenuItem fileMenuItem;
+
+    @FXML
+    private MenuItem dirMenuItem;
+
+    @FXML
+    private MenuItem closeMenuItem;
+
+    @FXML
+    private MenuItem aboutMenuItem;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		configureMenu();
+	}
+
+	private void configureMenu() {
+        closeMenuItem.setOnAction(x -> Platform.exit());
+ 
+        aboutMenuItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent arg0) {
+                Parent parent = null;
+                try {
+                    parent = FXMLLoader.load(getClass().getResource("/pl/javastart/mp3player/view/AboutPane.fxml"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Scene scene = new Scene(parent);
+                Stage stage = new Stage();
+                stage.setTitle("Mp3Player v1.0 - about");
+                stage.setScene(scene);
+                stage.show();
+            }
+        });
+    }
+	public Menu getFileMenu() {
+		return fileMenu;
+	}
+
+	public MenuItem getFileMenuItem() {
+		return fileMenuItem;
+	}
+
+	public MenuItem getDirMenuItem() {
+		return dirMenuItem;
+	}
+
+	public MenuItem getCloseMenuItem() {
+		return closeMenuItem;
+	}
+
+	public MenuItem getAboutMenuItem() {
+		return aboutMenuItem;
+	}
+
+}
